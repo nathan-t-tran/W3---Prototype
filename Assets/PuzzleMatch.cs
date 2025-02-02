27f1cd1c1658e6030b2d92642ff59998bcf338eb
Nathan Tran <nathantt@usc.edu>
2025-02-02 14:14:59 -0800

    made some changes to test colliders

--- Assets/PuzzleMatch.cs
+++ Assets/PuzzleMatch.cs
@@ -1,11 +1,13 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PuzzleMatch : MonoBehaviour
 {
+    public GameObject puzzleSolved;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        puzzleSolved.SetActive(false);
     }
 
     // Update is called once per frame
@@ -13,4 +15,22 @@ public class PuzzleMatch : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        Debug.Log("i have collided");
+        puzzleSolved.SetActive(true);
+    }
+
+    private void OnCollisionStay (Collision collision)
+    {
+        
+    }
+
+    private void OnCollisionExit (Collision collision)
+    {
+        Debug.Log("i am not collided");
+        puzzleSolved.SetActive(false);
+    }
+    
 }
